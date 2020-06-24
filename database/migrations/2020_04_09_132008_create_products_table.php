dd59cf234e4e4ab208f6ebdb8137c1e4970e975e
jeffwebsys <jeffwebsys@gmail.com>
2020-06-24 15:59:48 +0300

    Added

--- database/migrations/2020_04_09_132008_create_products_table.php
+++ database/migrations/2020_04_09_132008_create_products_table.php
@@ -41,43 +41,9 @@ class CreateProductsTable extends Migration
         Schema::table('subcategories', function($table) {
         $table->foreign('subcategory_id')->references('id')->on('subcategories')->onDelete('cascade');
     });
-        //table subcategories
-        Schema::create('subcategories', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedInteger('category_id');
-            $table->unsignedInteger('user_id');
-            $table->string('name');
-            $table->string('description');
-            $table->unsignedInteger('status');
-            $table->timestamps();
+        
 
-            // $table->foreign('category_id')
-            // ->references('id')
-            // ->on('categories')
-            // ->onDelete('cascade');
-        });
-
-         Schema::table('categories', function($table) {
-        $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-    });
-
-        //table categories
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->unsignedInteger('user_id');
-            $table->unsignedInteger('status');
-            $table->string('name');
-            $table->string('description');
-
-            // $table->foreign('user_id')
-            // ->references('id')
-            // ->on('users')
-            // ->onDelete('cascade');
-        });
-        Schema::table('users', function($table) {
-        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-    });
+        
 
  }
 
